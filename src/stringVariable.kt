72ba5346d6e0282f46f4116b0b554adac9b13ca3
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-02 17:58:10 +0530

    Basic string program

---
+++ src/stringVariable.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var string = "vyom"
+
+    println("Default String : $string")
+    println("Current String : ${string.uppercase()}")
+}
---
+++ src/volumeOfSphere.kt
@@ -0,0 +1,10 @@
+
+
+fun main(){
+    var radius = 5.5F
+    var pi = 3.14F
+
+    var volume = 0.75*pi*radius*radius*radius
+
+    print("The Volume of the sphere having radius $radius is : $volume")
+}
