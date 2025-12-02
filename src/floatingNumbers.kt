2797172debf281357ce016c0107cf92bc10900a6
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-02 17:00:25 +0530

    Floating var program

---
+++ src/floatingNumbers.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var x:Float = 20F
+    var y = 8F
+
+    println("The result of $x / $y is : ${x/y}")
+
+    var a = 9.0
+    var b = 2.0
+
+    print("The result of $a / $b is : ${a/b}")
+}
