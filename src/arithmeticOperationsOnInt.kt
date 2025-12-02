fa288f03de59520e846eb1877b8fb66aa3822cd7
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-02 12:48:00 +0530

    Initial commit

---
+++ src/arithmeticOperationsOnInt.kt
@@ -0,0 +1,13 @@
+
+
+fun main(){
+
+    val x=15
+    val y=6
+
+    println("The sum of $x and $y is : ${x + y}")
+    println("The difference of $x and $y is : ${x - y}")
+    println("The product of $x and $y is : ${x * y}")
+    println("The division of $x and $y is : ${x / y}")
+
+}
---
+++ src/variableAndValue.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var myVariable : Int = 5 //value can be changed
+    println("The value of myVariable is $myVariable")
+
+    myVariable = 8
+    println("The new value of myVariable is $myVariable")
+
+    val myValue = 6  //value cannot be changed further in the program
+    println("The value of myValue is $myValue")
+}
