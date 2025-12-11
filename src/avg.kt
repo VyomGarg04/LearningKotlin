e8804a1235bea749eef0650e48b1c00d1b781dc1
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-11 16:38:08 +0530

    changes in average program

--- src/avg.kt
+++ src/avg.kt
@@ -2,13 +2,18 @@
 
 fun main(){
     println("Enter 5 numbers")
-    val a = readln().toInt()
-    val c = readln().toInt()
-    val b = readln().toInt()
-    val d = readln().toInt()
-    val e = readln().toInt()
+//    val a = readln().toInt()
+//    val c = readln().toInt()
+//    val b = readln().toInt()
+//    val d = readln().toInt()
+//    val e = readln().toInt()
+    var avg:Float = 0.0f
+    for(i in 1..5){
+        var input = readln().toInt()
+        avg += input/5.0f
+    }
 
-    val avg = (a+b+c+d+e)/5
+//    val avg = (a+b+c+d+e)/5
     println("Average value is $avg")
 
 }
