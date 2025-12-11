74c5db4a056b143ee31f948621b2fff8c1aa3aa1
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-11 23:35:59 +0530

    Commit this change

--- src/reverseList.kt
+++ src/reverseList.kt
@@ -8,7 +8,12 @@ fun main() {
         list.add(x)
     }
     println("The 5 numbers in reverse order : ")
-    for (i in list.reversed())
-        println(i)
+//    for (i in list.reversed())
+//        println(i)
+
+    for(i in list.size -1 downTo 0){
+        println(list[i])
+
+    }
 
 }
