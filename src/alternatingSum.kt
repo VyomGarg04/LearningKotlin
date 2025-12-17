4f2f5dc7f3c1f3da0699f39abff3d328c18f442c
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-17 13:00:11 +0530

    alternating sum

--- src/alternatingSum.kt
+++ src/alternatingSum.kt
@@ -1,20 +1,19 @@
-
-
-fun main(){
-    println("Enter a number")
-    val n = readln().toInt()
-    val sum = alternatingSum(n)
-
-    println("The alternating sum from 1 to $n is $sum")
-}
-
-fun alternatingSum(n:Int):Int{
-    var sum =0
-    for(i in 1..n){
-        if(i%2==0)
+fun alternatingSum(vararg numbers: Int):Int{
+    var sum = 0
+    var j=0
+    for(i in numbers){
+        if(j%2==0)
             sum += i
         else
             sum -= i
+        j++
     }
     return sum
+}
+fun main(){
+    val arr = intArrayOf(261,84,3,1,45)
+
+    val sum = alternatingSum(1,2,3,5,7,9,*arr,13,6,3,2,93)
+
+    println("The alternating sum of the numbers is : $sum")
 }
