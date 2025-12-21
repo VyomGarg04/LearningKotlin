ccbfad0fe185ed1f5480d1893bb684945663dcae
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-21 23:17:35 +0530

    lambda function,filter function, custom filter,custom sum

---
+++ src/customSum.kt
@@ -0,0 +1,26 @@
+
+
+fun main(){
+
+    var list = (1..10).toList()
+    println(list)
+
+    list = list.filter { it % 2 == 1 }
+    println(list)
+
+    val sum  = list.customSum { it % 2 == 1}
+    println("Sum = $sum")
+
+
+}
+
+fun List<Int>.customSum(filterFunction: (Int) -> (Boolean)): Int {
+    val resultList = mutableListOf<Shape>()
+    var sum=0
+    for (i in this) {
+        if (filterFunction(i))
+            sum += i
+
+    }
+    return sum
+}
